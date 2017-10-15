c433798610ee9cd7319203bda17ceecc0ca59f95
mafuyuk <mafuyuk.m@gmail.com>
2017-10-15 14:39:03 +0900

    Add plot

--- line_plot.py
+++ line_plot.py
@@ -53,3 +53,9 @@ while diff > 1e-2:
   count += 1
   log = '{}回目 : theta0 = {:.3f}, theta1 = {:.3f}, 差分 = {:.4f}'
   print(log.format(count, theta0, theta1, diff))
+
+x = np.linspace(-3, 3, 100)
+plt.plot(train_z, train_y, 'o')
+plt.plot(x, f(x))
+plt.show()
+

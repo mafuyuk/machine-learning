7b59a62fa4bf3baf07912c7cc17466ed8ffaeb8d
mafuyuk <mafuyuk.m@gmail.com>
2017-10-22 18:11:52 +0900

    Add plot type

--- data-scientist-yousei-dokuhon/matplotlib/contour_color.py
+++ data-scientist-yousei-dokuhon/matplotlib/contour_color.py
@@ -8,3 +8,9 @@ X,Y=np.meshgrid(x,y)
 Z=X.ravel()**2-Y.ravel()**2
 plt.contourf(X,Y,Z.reshape(X.shape))
 plt.show()
+
+plt.contourf(X,Y,Z.reshape(X.shape),levels=[-20,-10,0,10,20],colors=["r","g","b","y"])
+plt.show()
+
+plt.contour(X,Y,Z.reshape(X.shape), colors="k")
+plt.show()
